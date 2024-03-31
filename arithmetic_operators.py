8a49a3a9713952015022f52ee8386065b6c4523b
Robert Parra <10241433+robert-ciro@users.noreply.github.com>
2024-03-31 17:24:19 +0200

    learned arithmetics

---
+++ arithmetic_operators.py
@@ -0,0 +1,6 @@
+print("Addition: 1+1 =", 1 + 1)  # 2
+print("subtraction: 1-1 =", 1 - 1)  # 0
+print("multiplication: 2*2 =", 2 * 2)  # 4
+print("true division: 27/4 =", 27 / 4)  # 6.75
+print("integer division: 27//4 =", 27 // 4)  # 6
+print("modulo operator", 4 % 2)  # 2
