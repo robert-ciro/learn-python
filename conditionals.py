175d678babb55db1a8443fc902702e07fdfc279d
Robert Parra <10241433+robert-ciro@users.noreply.github.com>
2024-03-31 17:24:24 +0200

    learned conditionals

---
+++ conditionals.py
@@ -0,0 +1,15 @@
+doorOpen = True
+
+if doorOpen:
+    print("Door open")
+else:
+    print("door not open")
+
+age = 33
+
+if age < 3:
+    print("It is a baby")
+elif age < 18:
+    print("It is not Adult")
+elif age >= 18:
+    print("It is Adult")
