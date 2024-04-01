9f02bdf73185cdd0fb9449c1cdf7bd5b7fc09156
Robert Parra <10241433+robert-ciro@users.noreply.github.com>
2024-04-01 11:00:25 +0200

    learned loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+on = True
+
+while on:
+    print("Turning off")
+    on = False
+
+for number in range(1, 10):
+    print(number)
+
+
+for number in range(1, 10):
+    if number > 8:
+        break
+    print(number)
